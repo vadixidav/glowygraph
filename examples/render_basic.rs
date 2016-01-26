e7233fb3e638b631d8bf1b5a5a00b49f8c227d00
vadix <vadixidav@gmail.com>
2016-01-26 11:53:57 -0500

    made proper API

--- examples/render_basic.rs
+++ examples/render_basic.rs
@@ -2,15 +2,37 @@ extern crate glowygraph;
 extern crate zoom;
 extern crate petgraph;
 extern crate glium;
+extern crate num;
+
+use num::traits::Float;
 
 fn main() {
     use glium::DisplayBuild;
-    let mut display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    let glowy = glowygraph::GraphRenderer::new(&display);
 
     let mut deps = petgraph::Graph::<zoom::Cartesian2<f32>, bool>::new();
-    deps.add_node(zoom::Cartesian2{x: -0.5, y: -0.5});
-    deps.add_node(zoom::Cartesian2{x: 0.0, y: 0.5});
-    deps.add_node(zoom::Cartesian2{x: 0.5, y: -0.25});
-    deps.add_node(zoom::Cartesian2{x: 0.25, y: -0.35});
-    glowygraph::render_nodes(&mut display, deps.node_weights_mut().map(|n| n.clone()));
+    deps.add_node(zoom::Cartesian2{x: -0.5, y: -0.5} * 32.0);
+    deps.add_node(zoom::Cartesian2{x: 0.0, y: 0.5} * 32.0);
+    deps.add_node(zoom::Cartesian2{x: 0.5, y: -0.25} * 32.0);
+    deps.add_node(zoom::Cartesian2{x: 0.25, y: -0.35} * 32.0);
+
+    let mut t = 0.0;
+    loop {
+        t += 0.01;
+        let transform = [
+            [t.cos()/32.0, -t.sin()/32.0, 0.0, 0.0],
+            [t.sin()/32.0, t.cos()/32.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ];
+        glowy.render(transform, deps.node_weights_mut().map(|n| n.clone()));
+
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,80 +6,76 @@ extern crate num;
 
 use glium::Surface;
 
-pub fn render_nodes<I>(display: &mut glium::Display, nodes: I)
-    where I: Iterator<Item=zoom::Cartesian2<f32>>
-{
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-        center: [f32; 2],
+static VSHADER_SOURCE: &'static str = r#"
+    #version 140
+    in vec2 position;
+    in vec2 center;
+    out vec2 frag_pos;
+    out vec2 frag_cen;
+    uniform mat4 matrix;
+    void main() {
+        frag_pos = position;
+        frag_cen = center;
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
+    }
+"#;
+
+static FSHADER_SOURCE: &'static str = r#"
+    #version 140
+    in vec2 frag_pos;
+    in vec2 frag_cen;
+    out vec4 color;
+    void main() {
+        color = vec4(1.0, 0.0, 0.0, max(0.0, 1.0 - length(frag_pos-frag_cen)));
+    }
+"#;
+
+pub struct GraphRenderer<'a> {
+    display: &'a glium::Display,
+    program: glium::Program,
+}
+
+impl<'a> GraphRenderer<'a> {
+    pub fn new(display: &'a glium::Display) -> Self {
+        GraphRenderer {
+            display: display,
+            program: glium::Program::from_source(display, VSHADER_SOURCE, FSHADER_SOURCE, None).unwrap(),
+        }
     }
 
-    implement_vertex!(Vertex, position, center);
-
-    let out_triangle = [
-        zoom::Cartesian2{x: 0.0, y: 2.0}/32.0,
-        zoom::Cartesian2{x: -1.7320508075689, y: -1.0}/32.0,
-        zoom::Cartesian2{x: 1.7320508075689, y: -1.0}/32.0,
-    ];
-
-    let vertices: Vec<_> = nodes.flat_map(|n| {
-        out_triangle
-            .iter()
-            .map(move |&v| (v + n))
-            .map(move |v| Vertex{position: [v.x, v.y], center: [n.x, n.y]})
-    }).collect();
-
-    let vertex_buffer = glium::VertexBuffer::new(display, &vertices).unwrap();
-    //let center_buffer = glium::VertexBuffer::new(display, &centers).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    let vertex_shader_src = r#"
-        #version 140
-        in vec2 position;
-        in vec2 center;
-        out vec2 frag_pos;
-        out vec2 frag_cen;
-        uniform mat4 matrix;
-        void main() {
-            frag_pos = position;
-            frag_cen = center;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 140
-        in vec2 frag_pos;
-        in vec2 frag_cen;
-        out vec4 color;
-        void main() {
-            color = vec4(1.0, 0.0, 0.0, max(0.0, 1.0 - 32.0*length(frag_pos-frag_cen)));
-        }
-    "#;
-
-    let program = glium::Program::from_source(display, vertex_shader_src,
-        fragment_shader_src, None).unwrap();
-
-    let mut t = -0.5;
-
-    loop {
-        // we update `t`
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
+    pub fn render<I>(&self, transform: [[f32; 4]; 4], nodes: I)
+        where I: Iterator<Item=zoom::Cartesian2<f32>>
+    {
+        #[derive(Copy, Clone)]
+        struct Vertex {
+            position: [f32; 2],
+            center: [f32; 2],
         }
 
-        let mut target = display.draw();
+        implement_vertex!(Vertex, position, center);
+
+        let out_triangle = [
+            zoom::Cartesian2{x: 0.0, y: 2.0},
+            zoom::Cartesian2{x: -1.7320508075689, y: -1.0},
+            zoom::Cartesian2{x: 1.7320508075689, y: -1.0},
+        ];
+
+        let vertices: Vec<_> = nodes.flat_map(|n| {
+            out_triangle
+                .iter()
+                .map(move |&v| (v + n))
+                .map(move |v| Vertex{position: [v.x, v.y], center: [n.x, n.y]})
+        }).collect();
+
+        let vertex_buffer = glium::VertexBuffer::new(self.display, &vertices).unwrap();
+        //let center_buffer = glium::VertexBuffer::new(display, &centers).unwrap();
+        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+        let mut target = self.display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
 
         let uniforms = uniform! {
-            matrix: [
-                [1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [ t , 0.0, 0.0, 1.0f32],
-            ]
+            matrix: transform,
         };
 
         let params = glium::DrawParameters {
@@ -87,15 +83,8 @@ pub fn render_nodes<I>(display: &mut glium::Display, nodes: I)
             ..Default::default()
         };
 
-        target.draw(&vertex_buffer, &indices, &program, &uniforms,
+        target.draw(&vertex_buffer, &indices, &self.program, &uniforms,
                     &params).unwrap();
         target.finish().unwrap();
-
-        for ev in display.poll_events() {
-            match ev {
-                glium::glutin::Event::Closed => return,
-                _ => ()
-            }
-        }
     }
 }
